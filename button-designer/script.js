3cd4bc5b3fd7e899219fb58a286c36505ff18a09
Manashi Roy <manashi.roy@icloud.com>
2024-02-05 21:38:33 +0530

    button designer implementation

---
+++ button-designer/script.js
@@ -0,0 +1,12 @@
+function updateButtonStyle(){
+   let text  = document.getElementById("text");
+   let button = document.getElementById("customButton");
+   let color = document.getElementById("color");
+   let backgroundColor = document.getElementById("background");
+   let borderRadius = document.getElementById('borderRadius');
+
+   button.innerHTML = text.value;
+   button.style.color = color.value;
+   button.style.backgroundColor = backgroundColor.value;
+   button.style.borderRadius = borderRadius.value + "px";
+}
