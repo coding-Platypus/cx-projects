b0e5b42eb69c63fb1431047a8e99d3af317de288
Manashi Roy <manashi.roy@icloud.com>
2024-02-06 10:57:07 +0530

    improvements and optimisation

--- button-designer/script.js
+++ button-designer/script.js
@@ -1,10 +1,10 @@
-function updateButtonStyle(){
-   let text  = document.getElementById("text");
-   let button = document.getElementById("customButton");
-   let color = document.getElementById("color");
-   let backgroundColor = document.getElementById("background");
-   let borderRadius = document.getElementById('borderRadius');
+let text  = document.getElementById("text");
+let button = document.getElementById("customButton");
+let color = document.getElementById("color");
+let backgroundColor = document.getElementById("background");
+let borderRadius = document.getElementById('borderRadius');
 
+function updateButtonStyle(){
    button.innerHTML = text.value;
    button.style.color = color.value;
    button.style.backgroundColor = backgroundColor.value;
