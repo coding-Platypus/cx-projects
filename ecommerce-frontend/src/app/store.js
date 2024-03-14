eddc25fa6085c89c31e52bd38d82291a89d12ff1
Manashi Roy <126547180+coding-Platypus@users.noreply.github.com>
2024-03-14 13:40:57 +0530

    Delete ecommerce-frontend directory

--- ecommerce-frontend/src/app/store.js
+++
@@ -1,6 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import productReducer from '../features/products/productSlice';
-
-export const store = configureStore({
-    reducer: productReducer
-})
